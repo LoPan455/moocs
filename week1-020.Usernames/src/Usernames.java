bb06a3a5c150baf20ed469ac0fade6e437fd19d8
Thomas Johander <thomas.johnader@target.com>
2019-02-11 12:48:16 -0600

    Finished Leap Year

--- week1-020.Usernames/src/Usernames.java
+++ week1-020.Usernames/src/Usernames.java
@@ -4,7 +4,26 @@ import java.util.Scanner;
 public class Usernames {
 
     public static void main(String[] args) {
+
         Scanner reader = new Scanner(System.in);
+        final String LOGGED_IN = "You are now logged into the system!";
+        final String LOGIN_FAILURE = "Your username or password are invalid!";
+        final String ALEX_PASSWORD = "mightyducks";
+        final String EMILY_PASSWORD = "cat";
+
+        System.out.println("Type your username: ");
+        String userName = reader.nextLine();
+
+        System.out.println("Type your password: ");
+        String password = reader.nextLine();
+
+
+
+        if (((userName.equals("alex") && password.equals(ALEX_PASSWORD)) || ((userName.equals("emily") && password.equals(EMILY_PASSWORD))))) {
+            System.out.println(LOGGED_IN);
+        } else {
+            System.out.println(LOGIN_FAILURE);
+        }
 
     }
 }
--- week1-021.LeapYear/src/LeapYear.java
+++ week1-021.LeapYear/src/LeapYear.java
@@ -1,10 +1,26 @@
-
 import java.util.Scanner;
 
 public class LeapYear {
 
-    public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
 
-    }
+		final String leapYear = "The year is a leap year";
+		final String notLeapYear = "The year is not a leap year";
+		String returnString;
+
+		int year = Integer.parseInt(reader.nextLine());
+
+		if ((year % 100 != 0 && year % 400 == 0) || (year % 4 == 0))
+		{
+			returnString = leapYear;
+			if (year % 100 == 0 && year % 400 != 0) {
+				returnString = notLeapYear;
+			}
+		} else {
+			returnString = notLeapYear;
+		}
+		System.out.println(returnString);
+	}
+
 }
