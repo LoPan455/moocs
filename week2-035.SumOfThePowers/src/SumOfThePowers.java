11dc4e716b6afebfc8e2c9d7b72dedd01ab22798
Thomas Johander <thomas.johnader@target.com>
2019-02-14 12:24:56 -0600

    Completed exercise 35

--- week2-035.SumOfThePowers/src/SumOfThePowers.java
+++ week2-035.SumOfThePowers/src/SumOfThePowers.java
@@ -6,5 +6,18 @@ public class SumOfThePowers {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type a number: ");
+        int input = Integer.parseInt(reader.nextLine());
+        int sum = 0;
+        int iterator = 0;
+
+        while (iterator <= input) {
+            sum += (int) Math.pow(2, iterator);
+            iterator++;
+        }
+
+        System.out.println("The sum is: " +  sum);
+
+
     }
 }
